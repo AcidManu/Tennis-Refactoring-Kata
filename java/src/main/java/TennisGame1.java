72d5250eee510bee414fab4bd3bfe452389d8a63
Manuel Andrés Carrera Galafate <manuelcarrera@acidtango.com>
2023-09-29 15:51:44 +0100

    feat: renames some magic strings

--- java/src/main/java/TennisGame1.java
+++ java/src/main/java/TennisGame1.java
@@ -1,6 +1,10 @@
-
 public class TennisGame1 implements TennisGame {
-    
+
+    public static final int POINT_VALUE = 1;
+    public static final String ZERO_POINTS_STRING = "Love";
+    public static final String ONE_POINT_STRING = "Fifteen";
+    public static final String TWO_POINTS_STRING = "Thirty";
+    public static final String THREE_POINTS_STRING = "Forty";
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -12,10 +16,18 @@ public class TennisGame1 implements TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == player1Name)
-            m_score1 += 1;
+        if ((playerName.equals(player1Name)))
+            addPointToPlayer1();
         else
-            m_score2 += 1;
+            addPointToPlayer2();
+    }
+
+    private void addPointToPlayer2() {
+        m_score2 += POINT_VALUE;
+    }
+
+    private void addPointToPlayer1() {
+        m_score1 += POINT_VALUE;
     }
 
     public String getScore() {
@@ -40,7 +52,7 @@ public class TennisGame1 implements TennisGame {
 
             }
         }
-        else if (m_score1>=4 || m_score2>=4)
+        else if (isAdvantageGame())
         {
             int minusResult = m_score1-m_score2;
             if (minusResult==1) score = new StringBuilder("Advantage player1");
@@ -57,21 +69,25 @@ public class TennisGame1 implements TennisGame {
                 switch(tempScore)
                 {
                     case 0:
-                        score.append("Love");
+                        score.append(ZERO_POINTS_STRING);
                         break;
                     case 1:
-                        score.append("Fifteen");
+                        score.append(ONE_POINT_STRING);
                         break;
                     case 2:
-                        score.append("Thirty");
+                        score.append(TWO_POINTS_STRING);
                         break;
                     case 3:
-                        score.append("Forty");
+                        score.append(THREE_POINTS_STRING);
                         break;
                 }
             }
         }
         return score.toString();
+    }
+
+    private boolean isAdvantageGame() {
+        return m_score1 >= 4 || m_score2 >= 4;
     }
 
     private boolean isATie() {
